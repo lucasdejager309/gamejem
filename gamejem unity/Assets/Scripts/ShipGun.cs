ba7b53e75bd8a85162f91d3bea173e6b6d0a1e5e
Marten Hilhorst <martenh88888@gmail.com>
2022-01-29 14:04:45 +0100

    Gun turret and angle limit

--- gamejem unity/Assets/Scripts/ShipGun.cs
+++ gamejem unity/Assets/Scripts/ShipGun.cs
@@ -6,34 +6,53 @@ public class ShipGun : MonoBehaviour
 {
     public float RotationSpeed;
     public float ShootCooldown;
+    private float CurrentCooldown = 0;
     public float Bulletspeed;
+    public float RotationRange;
+    private float MinRotationRange;
 
+    public GameObject BulletPrefab;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        MinRotationRange = 360 - RotationRange;
     }
 
     private void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.J))
-        {
+        { 
             //transform.position += new Vector3(TargetMoveSpeed, 0, 0);
             transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + RotationSpeed);
         }
-        if (Input.GetKey(KeyCode.L))
+        if (Input.GetKey(KeyCode.L))  //&& Mathf.Abs(( transform.localRotation.eulerAngles.z - 360) - RotationSpeed) < RotationRange)
         {
             transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z - RotationSpeed);
+                
         }
 
+        if(transform.localRotation.eulerAngles.z > RotationRange && transform.localRotation.eulerAngles.z < 180)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, RotationRange);
+        }
+        else if(transform.localRotation.eulerAngles.z < MinRotationRange && transform.localRotation.eulerAngles.z > 180)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, MinRotationRange);
+        }
+
+        print(transform.localRotation.eulerAngles.z);
+        if (ShootCooldown < CurrentCooldown) {
+            if (Input.GetKey(KeyCode.RightShift))
+            {
+                GameObject Bullet = Instantiate(BulletPrefab, transform.position, transform.rotation);
+                Bullet.GetComponent<Rigidbody2D>().velocity = Bullet.GetComponent<Transform>().up * Bulletspeed;
+                CurrentCooldown = 0;
+            }
+        }
+        else
+        {
+            CurrentCooldown += Time.deltaTime;
+        }
 
     }
 
