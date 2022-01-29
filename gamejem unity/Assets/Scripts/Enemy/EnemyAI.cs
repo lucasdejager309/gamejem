3ddc8ea64a191e48bf3a9487e431dd0e5a8f003d
Lucas de Jager <lucasdejager@gmail.com>
2022-01-29 02:16:02 +0100

    enemy orbits player and shoots pew pew

--- gamejem unity/Assets/Scripts/Enemy/EnemyAI.cs
+++ gamejem unity/Assets/Scripts/Enemy/EnemyAI.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     public string targetTag;
-    public float attackDistance;
+    public float minOrbitDistance;
+    public float maxOrbitDistance;
+    public float attackRange;
+    public float attackSpeed;
     public float rotationSpeed;
-    public float chargeRotSpread;
     public float movementSpeed;
     public float acceleration;
+    public Transform bulletOrigin;
+    public GameObject bulletPrefab;
+    public float bulletSpread;
+    public float bulletSpeed;
     GameObject target;
     
     float currentDistance;
+    float sinceLastShot;
 
     Rigidbody2D rb;
 
     enum enemyState {
         charge,
-        circle
+        circle,
+        flee
     }
 
     enemyState state;
@@ -30,55 +38,95 @@ public class EnemyAI : MonoBehaviour
 
     void FixedUpdate()
     {
-
         currentDistance = Vector2.Distance(transform.position, target.transform.position);
 
         if (target != null) {
             switch (state) {
                 case enemyState.charge:
                     Charge();
-                    if (currentDistance < attackDistance) {
+                    if (currentDistance < maxOrbitDistance) {
+                        rb.velocity = new Vector2(0,0);
                         state = enemyState.circle;
+                    } else if (currentDistance < minOrbitDistance) {
+                        state = enemyState.flee;
                     }
                     break;
                 case enemyState.circle:
                     CircleTarget();
-                    if (currentDistance > attackDistance) {
+                    if (currentDistance > maxOrbitDistance+maxOrbitDistance/4) {
+                        state = enemyState.charge;
+                    } else if (currentDistance < minOrbitDistance) {
+                        state = enemyState.flee;
+                    }
+                    break;
+
+                case enemyState.flee:
+                    Flee();
+                    if (currentDistance > minOrbitDistance) {
                         state = enemyState.charge;
                     }
                     break;
             }
         }
+
+        Move();
+
+        if (currentDistance < attackRange) {
+            Attack();
+        }
+
+        if (sinceLastShot > 0) {
+            sinceLastShot -= Time.deltaTime;
+        } else if (sinceLastShot < 0) {
+            sinceLastShot = 0;
+        }
     }
 
     void Charge() {
         float eulerRotZ = Quaternion.ToEulerAngles(transform.rotation).z;
-        
-
-        rb.velocity -= new Vector2(transform.right.x, transform.right.y)*acceleration;
-        if (rb.velocity.magnitude > movementSpeed) {
-            rb.velocity = rb.velocity.normalized*movementSpeed;
-        }
-
-        
-
-        
-        RotateTowards(target.transform);
+ 
+        RotateTowards(target.transform.position);
         
     }
 
     void CircleTarget() {
-
+        float angle = getRotZ(target.transform.position)+50;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, angle), Time.deltaTime*rotationSpeed);
     }   
 
-    void RotateTowards(Transform point) {
+    void RotateTowards(Vector2 point) {
         float rotZ = getRotZ(point);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, rotZ), Time.deltaTime*rotationSpeed);
     }
 
-    float getRotZ(Transform point) {
-        Vector3 difference = transform.position - point.position;
+    float getRotZ(Vector2 point) {
+        Vector3 difference = new Vector2(transform.position.x, transform.position.y) - point;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         return rotZ;
+    }
+
+    void Move() {
+        rb.velocity -= new Vector2(transform.right.x, transform.right.y)*acceleration;
+        if (rb.velocity.magnitude > movementSpeed) {
+            rb.velocity = rb.velocity.normalized*movementSpeed;
+        }
+    }
+
+    void Flee() {
+        float angle = getRotZ(target.transform.position)+180;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 0f, angle), Time.deltaTime*rotationSpeed);
+    }
+
+    void Attack() {
+        if (sinceLastShot == 0) {
+            Debug.Log("shoot");
+
+            Vector3 difference = target.transform.position - bulletOrigin.position;
+            float rotZ = (Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg) + Random.Range(-bulletSpread, bulletSpread);
+            GameObject bullet = Instantiate(bulletPrefab, bulletOrigin.position, Quaternion.Euler(0f, 0f, rotZ));
+            bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.right*bulletSpeed;
+
+            sinceLastShot = attackSpeed;
+        }
     }
 }
